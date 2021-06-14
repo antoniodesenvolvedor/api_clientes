87d897c79e21fa46ef6830d0d06c7c46ec41243b
Antonio Batista <antoniodesenvolvedor@gmail.com>
2021-06-13 23:33:14 -0300

    Fix: Ajustando configuração para produção

--- src/server/config.py
+++ src/server/config.py
@@ -12,7 +12,7 @@ _POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 _POSTGRES_DB = os.getenv('POSTGRES_DB')
 _POSTGRES_PORT = os.getenv('POSTGRES_PORT')
 
-_postgres_sql_host = 'postgres_mercafaci' if PRODUCTION else 'localhost'
+_postgres_sql_host = 'postgres_mercafacil' if PRODUCTION else 'localhost'
 CONNECTION_STRING_POSTGRES = f'postgresql+psycopg2://{_POSTGRES_USER}:{_POSTGRES_PASSWORD}' \
                              f'@{_postgres_sql_host}:{_POSTGRES_PORT}/{_POSTGRES_DB}'
 
@@ -26,6 +26,8 @@ _MYSQL_PORT = os.getenv('MYSQL_PORT')
 _my_sql_host = 'mysql_mercafacil' if PRODUCTION else 'localhost'
 CONNECTION_STRING_MYSQL = f'mysql+mysqlconnector://{_MYSQL_USER}:{_MYSQL_PASSWORD}' \
                              f'@{_my_sql_host}:{_MYSQL_PORT}/{_MYSQL_DATABASE}'
+
+print(CONNECTION_STRING_MYSQL)
 
 
 TOKEN_KEY = os.getenv('TOKEN_KEY')
